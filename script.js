e823e67c604ffb01477128813f7a9c4bf1271a04
DNM Samarasinghe <39-bis-0006@kdu.ac.lk>
2023-10-17 08:34:30 +0530

    changes

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 $(document).ready(function(){
+
   $('#menu').click(function(){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+
   });
 
   $(window).on('scroll load',function(){
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
 
-    if ($(window).scrollTop() > 0) {
-      // Assuming you have an element with class .top that you want to show/hide
-      $('.top').show();
-    } else {
-      $('.top').hide();
-    }
   });
+
+  //smooth scrolling
+  $('a[href*="#"]').on('click',function(e){
+      e.preventDefault();
+
+      $('html,body').animate({
+
+          scrollTop : $($(this).attr('href')).offset().top,
+
+      },
+      500,
+      'linear'
+      
+      
+      );
+
+  })
+
 });
 
-const textElement = document.getElementById('animated-text');
-const text = textElement.textContent;
-let index = 0;
 
-function typeText() {
-  if (index < text.length) {
-    textElement.style.visibility = 'visible';
-    textElement.innerHTML = text.substring(0, index + 1);
-    index++;
-    setTimeout(typeText, 50); // Adjust the typing speed by changing the delay (e.g., 50ms)
-  }
-}
+/* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText", {
+    strings: ["undergraduate", "Developer", "Business Analyst"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay:2000,
+  });
 
-// Trigger the typing animation when the page loads
-window.addEventListener('load', typeText);
 
- 
+
+
+
+
+
+
+
+
